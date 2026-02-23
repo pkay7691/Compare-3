d267a6f80080c717c008d97315f5305a8ba9ccb6
Pat Kenny <p.kenny@ompimail.com>
2026-02-23 15:28:13 -0600

    added link to logo

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -9,7 +9,7 @@ import Image from "next/image";
 const Navbar = () => {
     return ( 
         <nav className="flex flex-row gap-20 border-b-2 border-gray-200 items-center p-4">
-            <Image src="/Compare Black.svg" alt="Logo" width={100} height={100} />
+            <Link href="/"><Image src="/Compare Black.svg" alt="Logo" width={100} height={100} /></Link>
             <ul className="flex flex-row gap-20 items-center">
                 <li><Link href="/">Home</Link></li>
                 <li><Link href="/about ">About</Link></li>
